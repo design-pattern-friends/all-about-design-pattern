d9f84a92e9c9af794abca4fca0cb9ca1a739bed7
nerdchanii <nerdchanii@gmail.com>
2022-11-20 01:56:58 +0900

    chore(settings): setup monorepo(#2)
    - choose pacakge manger - `yarn Berry`
      - not use zero-install
    - set monorepo
    - use typescript
    - use eslint (with air-bnb)
    - use prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {},
+};
