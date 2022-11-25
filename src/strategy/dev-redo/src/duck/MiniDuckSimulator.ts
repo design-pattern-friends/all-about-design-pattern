ce910d666f93df5d9b74c07ebc859834ba7b8881
dev-redo <mis05041@naver.com>
2022-11-26 00:34:52 +0900

    feat(dev-redo/strategy): 오리 게임 실행 코드 구현

--- src/strategy/dev-redo/src/duck/MiniDuckSimulator.ts
+++ src/strategy/dev-redo/src/duck/MiniDuckSimulator.ts
@@ -1,5 +1,5 @@
 import { Duck } from './Duck';
-import { FlyWithWing } from './FlyBehavior/FlyWithWing';
+import { FlyWithWings } from './FlyBehavior/FlyWithWings';
 import { Quack } from './QuackBehavior/Quack';
 import { ModelDuck } from './ModelDuck';
 import { FlyRocketPowered } from './FlyBehavior/FlyRocketPowered';
@@ -7,19 +7,17 @@ import { FlyRocketPowered } from './FlyBehavior/FlyRocketPowered';
 export class MiniDuckSimulator extends Duck {
   constructor() {
     super();
-    this.flyBehavior = new FlyWithWing();
+    this.flyBehavior = new FlyWithWings();
     this.quackBehavior = new Quack();
 
     this.performFly();
     this.performQuack();
-
-    this.model = new ModelDuck();
-    this.model.performFly();
-    this.model.setFlyBehavior(new FlyRocketPowered());
-    this.model.performFly();
   }
 
-  public display(): void {
-    console.log('저는 모형오리입니다.');
+  setSimulatorToModel() {
+    const model = new ModelDuck();
+    model.performFly();
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
   }
 }
