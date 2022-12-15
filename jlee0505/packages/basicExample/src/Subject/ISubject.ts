d26c8c230efc3394c046fb4f2c5ac20b609338d9
jlee0505 <hj960322@gmail.com>
2022-12-15 15:06:21 +0900

    chore: 초기 설정

--- jlee0505/packages/basicExample/src/Subject/ISubject.ts
+++
@@ -1,7 +0,0 @@
-import { IObserver } from "../Observer/IObserver";
-
-export interface ISubject {
-    registerObserver(observer: IObserver): void;
-    removeObserver(observer:IObserver): void;
-    notifyObservers(): void;
-}
