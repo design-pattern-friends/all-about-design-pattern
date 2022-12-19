33c6226d1fa25b6bf91829aec65ffe5c752be8f9
hodomaroo <enghemdwkd@gmail.com>
2022-12-19 22:40:00 +0900

    feat(hodomaroo/factory) 팩토리 패턴 예제 구현

--- hodomaroo/src/main/java/decorator/HouseDecorator.java
+++ hodomaroo/src/main/java/decorator/HouseDecorator.java
@@ -1,12 +1,14 @@
 package decorator;
 
 public abstract class HouseDecorator implements House {
+
     private House house;
 
 
-    public HouseDecorator(House house){
+    public HouseDecorator(House house) {
         this.house = house;
     }
+
     @Override
     public void description() {
         house.description();
