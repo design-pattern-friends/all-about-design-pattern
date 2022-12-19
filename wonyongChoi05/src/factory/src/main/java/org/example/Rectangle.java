2fced455884e7520417b960be2bf945d3b07d8ad
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-12-19 10:42:38 +0900

    feat(factory/shape): 팩토리 패턴을 활용한 모양 그리기

---
+++ wonyongChoi05/src/factory/src/main/java/org/example/Rectangle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author 최원용
+ * @version 2.0.0
+ * @Created by 최원용 on 2022. 12. 19.
+ */
+public class Rectangle extends UserFactory{
+    public void draw() {
+        super.shape().draw("사각형");
+    }
+}
