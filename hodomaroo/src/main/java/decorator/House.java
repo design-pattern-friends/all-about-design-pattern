ab0dba2bfbd31817234d425638d2a940532fe5e0
hodomaroo <enghemdwkd@gmail.com>
2022-12-19 22:46:04 +0900

    feat(Factory) 팩토리 패턴 과제 구현 완료

--- hodomaroo/src/main/java/decorator/House.java
+++ hodomaroo/src/main/java/decorator/House.java
@@ -1,10 +1,12 @@
 package decorator;
 
 public interface House {
+
     void description();
+
     int cost();
 
-    default void displayInfo(){
+    default void displayInfo() {
         description();
         System.out.println("견적 : " + cost() + "원\n");
     }
