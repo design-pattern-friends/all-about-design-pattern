3a857d220b383b949fe23c862ae1b9c1b7e39791
yeong bin <2976622@naver.com>
2023-01-01 12:10:54 +0900

    fix: CarFactory import 오타 수정

--- yeongbinim/packages/practice/factorymethod/index.ts
+++ yeongbinim/packages/practice/factorymethod/index.ts
@@ -1,12 +1,10 @@
-import Car from './Car/Car';
 import CarFactory from './CarFactory/CarFactory';
 import HyundaiCarFactory from './CarFactory/HyundaiCarFactory';
 import KiaCarFactory from './CarFactory/KiaCarFactory';
 
 let factory: CarFactory = new HyundaiCarFactory();
-const newHyundaiCar: Car | null = factory.createCar('sonata');
 
-console.log(newHyundaiCar !== null ? newHyundaiCar.toString() : newHyundaiCar);
+factory.sellCar('sonata');
 
 factory = new KiaCarFactory();
 factory.sellCar('k3');
