d443d3370baecbe168cc9ae5d26ad61b2746488d
dev-redo <mis05041@naver.com>
2023-01-05 03:03:42 +0900

    feat(strategy): 전략패턴을 이용한 로그인 예제 구현

---
+++ dev-redo/packages/strategy/login/src/auth/Authenticator.ts
@@ -0,0 +1,19 @@
+import { Strategy } from '../type/auth';
+
+class Authenticator {
+  strategies: Record<string, Strategy> = {};
+
+  use(name: string, strategy: Strategy) {
+    this.strategies[name] = strategy;
+  }
+
+  authenticate(name: string, ...args: any) {
+    if (!this.strategies[name]) {
+      console.error('Authentication policy has not been set!');
+      return false;
+    }
+    return this.strategies[name].authenticate.apply(null, args);
+  }
+}
+
+export { Authenticator };
---
+++ dev-redo/packages/strategy/login/src/strategy/TwitterStrategy.ts
@@ -0,0 +1,18 @@
+import { Strategy } from '../type/auth';
+
+const accesssToken = 'tw123';
+
+class TwitterStrategy implements Strategy {
+  authenticate(args: any[]) {
+    const [token] = args;
+    if (token !== accesssToken) {
+      console.error('Twitter :>> 트위터 계정 인증 실패!');
+      return false;
+    }
+
+    console.log('Twitter :>> 트위터 계정 인증 성공!');
+    return true;
+  }
+}
+
+export { TwitterStrategy };
